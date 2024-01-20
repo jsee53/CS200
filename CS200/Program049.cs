69ebdd240d3b8485b8c56bf0a2addc1f94175267
32184475 <32184475@dankook.ac.kr>
2024-01-20 23:58:45 +0900

    Feat: 041~050 예저 추가

---
+++ CS200/Program049.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CS200
+{
+    internal class Program049
+    {
+        public static void Solve049()
+        {
+            Console.WriteLine("숫자를 입력하세요: ");
+            int num = int.Parse(Console.ReadLine());
+            int index;
+
+            for (index = 2; index < num; index++)
+            {
+                if (num % index == 0)
+                {
+                    Console.WriteLine("{0}는 소수가 아닙니다", num);
+                    break;
+                }
+            }
+            if (index == num)
+                Console.WriteLine("{0}는 소수입니다", num);
+        }
+    }
+}
